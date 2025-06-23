05ad7fa0f136e14d6fff7f2154a88ac634c31332
Nils Hansson <nilsmenghansson@gmail.com>
2025-06-23 13:33:17 +0200

    smaller heading

--- src/app/components/home/hero/index.tsx
+++ src/app/components/home/hero/index.tsx
@@ -34,14 +34,13 @@ function HeroSection() {
             <div className="flex flex-col gap-6 Xxl:pb-20 pb-10">
               <div className="flex items-start gap-2 md:gap-6">
                 <div className="w-11 h-11 flex-shrink-0"></div>
-                <p className="text-white/70 max-w-md">
-                  {" "}
-                  <span className="text-primary">Flutist</span> based in
-                  Helsinki.
-                </p>
               </div>
               <div className="flex flex-col lg:flex-row items-start lg:items-end gap-4">
-                <h4 className="large-heading">Janette Lévan</h4>
+                <p className="text-white/70 max-w-md">
+                  {" "}
+                  <span className="text-primary">Flutist</span>
+                </p>
+                <h1 className="large-heading">Janette Lévan</h1>
                 <div></div>
               </div>
             </div>
